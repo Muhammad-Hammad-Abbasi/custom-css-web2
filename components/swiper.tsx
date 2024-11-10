88b29b01b2f441c10b2dad56bffc59f6e73e55b7
Muhammad-Hammad-Abbasi <hammadabbasi102030@gmail.com>
2024-11-09 23:07:43 -0800

    updade.

--- components/swiper.tsx
+++ components/swiper.tsx
@@ -4,108 +4,35 @@ import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/navigation';  
-import '@/css-styles/swiper-custom.css'; 
-import '@/css-styles/slider.css' 
+import 'swiper/css/navigation';
+
 
 
 export default function Swiper_img() {
     return (
         <Swiper
-            modules={[Navigation]}
-            spaceBetween={20}
-            slidesPerView={1}
-            breakpoints={{
-                640: { slidesPerView: 1, spaceBetween: 10 },
-                768: { slidesPerView: 2, spaceBetween: 20 },
-                1024: { slidesPerView: 2, spaceBetween: 20 },
-            }}
-            navigation 
-            className="swiper-slider"
-            loop={true}  
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-            
-        >
-            <SwiperSlide className='slide-padding'>
-                <Image                   
-                    src="/slider-img7.png"
-                    height={600}
-                    width={350}
-                    alt="Slide 1"
-                />
-            </SwiperSlide>
-
-            <SwiperSlide className='slide-padding'>
-                <Image
-                    src="/slider-img02.png"
-                    height={600}
-                    width={350}
-                    alt="Slide 2"
-                />
-            </SwiperSlide>
-
-            <SwiperSlide className='slide-padding'>
-                <Image
-                    src="/slider-img6.png"
-                    height={600}
-                    width={350}
-                    alt="Slide 3"
-                />
-            </SwiperSlide>
+        modules={[Navigation]}
+        spaceBetween={20}
+        slidesPerView={1}
+        breakpoints={{
+            640: { slidesPerView: 1, spaceBetween: 10 },
+            768: { slidesPerView: 2, spaceBetween: 20 },
+            1024: { slidesPerView: 2, spaceBetween: 20 },
+        }}
+        navigation
+        loop={true}
+    >
+        {/* Slides */}
+        {["/slider-img7.png", "/slider-img02.png", "/slider-img6.png", "/slider-img04.png", "/slider-img05.png", "/slider-img03.png", "/slider-image-1.png", "/slider-img8.png"].map((src, index) => (
+        <SwiperSlide key={index}>
+            <Image src={src} height={600} width={350} alt={`Slide ${index + 1}`} />
+        </SwiperSlide>
+    ))}
+    
+    </Swiper>
+   
 
             
-            <SwiperSlide className='slide-padding'>
-                <Image
-                    src="/slider-img04.png"
-                    height={600}
-                    width={350}
-                    alt="Slide 4"
-                />
-            </SwiperSlide>
-
-            
-            <SwiperSlide className='slide-padding'>
-                <Image
-                    src="/slider-img05.png"
-                    height={600}
-                    width={350}
-                    alt="Slide 5"
-                />
-            </SwiperSlide>
-
-
-            
-            <SwiperSlide className='slide-padding'>
-                <Image
-                    src="/slider-img03.png"
-                    height={600}
-                    width={350}
-                    alt="Slide 6"
-                />
-            </SwiperSlide>
-
-            
-            <SwiperSlide className='slide-padding'>
-                <Image
-                    src="/slider-image-1.png"
-                    height={600}
-                    width={350}
-                    alt="Slide 7"
-                />
-            </SwiperSlide>
-
-            
-            <SwiperSlide className='slide-padding'>
-                <Image
-                    src="/slider-img8.png"
-                    height={600}
-                    width={350}
-                    alt="Slide 8"
-                />
-            </SwiperSlide>
-
-            
-        </Swiper>
+     
     );
 }
