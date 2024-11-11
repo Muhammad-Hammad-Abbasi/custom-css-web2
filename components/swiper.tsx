32b03a164976c5483a8a0a176058e081b1e11272
Muhammad-Hammad-Abbasi <hammadabbasi102030@gmail.com>
2024-11-11 14:24:00 -0800

    update.

--- components/swiper.tsx
+++ components/swiper.tsx
@@ -15,7 +15,7 @@ export default function Swiper_img() {
         spaceBetween={20}
         slidesPerView={1}
         breakpoints={{
-            640: { slidesPerView: 1, spaceBetween: 20 },
+            640: { slidesPerView: 1, spaceBetween: 10 },
             768: { slidesPerView: 2, spaceBetween: 20 },
             1024: { slidesPerView: 2, spaceBetween: 20 },
         }}
